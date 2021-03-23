439572bdde0d8d811a054cba2d3cef0d72bd22de
Gilles CHEHADE <gilles@poolp.org>
2021-03-23 02:52:35 +0100

    Inflate and Deflate

---
+++ store/compression.go
@@ -0,0 +1,29 @@
+package store
+
+import (
+	"bytes"
+	"compress/gzip"
+	"io/ioutil"
+)
+
+func Deflate(buf []byte) []byte {
+	var b bytes.Buffer
+	w := gzip.NewWriter(&b)
+	w.Write(buf)
+	w.Close()
+	return b.Bytes()
+}
+
+func Inflate(buf []byte) ([]byte, error) {
+	w, err := gzip.NewReader(bytes.NewBuffer(buf))
+	if err != nil {
+		return nil, err
+	}
+	defer w.Close()
+
+	data, err := ioutil.ReadAll(w)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}

a727b80167df9d2086810bf145d1482cffa4f8a3
Gilles CHEHADE <gilles@poolp.org>
2021-03-28 20:57:37 +0200

    plakar push to ... plakar pull from ...

--- cmd/plakar/plakar.go
+++ cmd/plakar/plakar.go
@@ -37,6 +37,109 @@ var quiet bool
 const VERSION = "0.0.1"
 
 func main() {
+
+	hostbuf, err := os.Hostname()
+	if err != nil {
+		hostbuf = "localhost"
+	}
+
+	pwUser, err := user.Current()
+	if err != nil {
+		log.Fatalf("%s: user %s has turned into Casper", flag.CommandLine.Name(), pwUser.Username)
+	}
+
+	flag.StringVar(&storeloc, "store", fmt.Sprintf("%s/.plakar", pwUser.HomeDir), "data store")
+	flag.StringVar(&namespace, "namespace", "default", "storage namespace")
+	flag.StringVar(&hostname, "hostname", strings.ToLower(hostbuf), "local hostname")
+	flag.BoolVar(&quiet, "quiet", false, "quiet mode")
+
+	flag.Parse()
+
+	namespace = strings.ToLower(namespace)
+	hostname = strings.ToLower(hostname)
+
+	if len(flag.Args()) == 0 {
+		fmt.Println("valid subcommands:")
+		fmt.Println("\tcat <snapshot>:<file>")
+		fmt.Println("\tcat <snapshot>:<object>")
+		fmt.Println("\tcheck <snapshot> [<snapshot>]")
+		fmt.Println("\tdiff <snapshot> <snapshot>")
+		fmt.Println("\tdiff <snapshot> <snapshot> <file>")
+		fmt.Println("\tls <snapshot> <snapshot> <file>")
+		fmt.Println("\tpull <snapshot> [<snapshot> ...]")
+		fmt.Println("\tpush <path> [<path> ...]")
+		fmt.Println("\trm <snapshot> [<snapshot> ...]")
+		fmt.Println("\tversion")
+		log.Fatalf("%s: missing command", flag.CommandLine.Name())
+	}
+
+	command, args := flag.Arg(0), flag.Args()[1:]
+
+	if len(args) > 1 {
+		if command != "init" {
+			if command == "push" {
+				if args[len(args)-2] == "to" {
+					storeloc = args[len(args)-1]
+					args = args[:len(args)-2]
+				}
+			} else {
+				if args[len(args)-2] == "from" {
+					storeloc = args[len(args)-1]
+					args = args[:len(args)-2]
+				}
+			}
+		}
+	}
+
+	var store repository.Store
+	if strings.HasPrefix(storeloc, "plakar://") {
+		pstore := &client.ClientStore{}
+		pstore.Namespace = namespace
+		pstore.Repository = storeloc
+		store = pstore
+
+	} else {
+		pstore := &fs.FSStore{}
+		pstore.Namespace = namespace
+		pstore.Repository = storeloc
+		store = pstore
+	}
+	store.Init()
+
+	switch command {
+	case "cat":
+		cmd_cat(store, args)
+
+	case "check":
+		cmd_check(store, args)
+
+	case "diff":
+		cmd_diff(store, args)
+
+	case "ls":
+		cmd_ls(store, args)
+
+	case "pull":
+		cmd_pull(store, args)
+
+	case "push":
+		cmd_push(store, args)
+
+	case "rm":
+		cmd_rm(store, args)
+
+	case "server":
+		cmd_server(store, args)
+
+	case "version":
+		cmd_version(store, args)
+
+	default:
+		log.Fatalf("%s: unsupported command: %s", flag.CommandLine.Name(), command)
+	}
+}
+
+func main2() {
 	hostbuf, err := os.Hostname()
 	if err != nil {
 		hostbuf = "localhost"

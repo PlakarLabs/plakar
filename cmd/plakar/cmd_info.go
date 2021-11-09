80d45050a6d1a6bcb0fce085e5a523763c7bb34b
Gilles CHEHADE <gilles@poolp.org>
2021-11-09 13:51:17 +0100

    simplify cmd_info

--- cmd/plakar/cmd_info.go
+++ cmd/plakar/cmd_info.go
@@ -22,7 +22,6 @@ import (
 	"log"
 
 	"github.com/dustin/go-humanize"
-	"github.com/poolpOrg/plakar/snapshot"
 )
 
 func cmd_info(ctx Plakar, args []string) int {
@@ -33,37 +32,24 @@ func cmd_info(ctx Plakar, args []string) int {
 		log.Fatalf("%s: need at least one snapshot ID to pull", flag.CommandLine.Name())
 	}
 
-	snapshots := getSnapshotsList(ctx)
-
-	for i := 0; i < len(args); i++ {
-		prefix, _ := parseSnapshotID(args[i])
-		res := findSnapshotByPrefix(snapshots, prefix)
-		if len(res) == 0 {
-			log.Fatalf("%s: no snapshot has prefix: %s", flag.CommandLine.Name(), prefix)
-		} else if len(res) > 1 {
-			log.Fatalf("%s: snapshot ID is ambigous: %s (matches %d snapshots)", flag.CommandLine.Name(), prefix, len(res))
-		}
+	snapshots, err := getSnapshots(ctx.Store(), flags.Args())
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	for i := 0; i < len(args); i++ {
-		prefix, _ := parseSnapshotID(args[i])
-		res := findSnapshotByPrefix(snapshots, prefix)
-		snap, err := snapshot.Load(ctx.Store(), res[0])
-		if err != nil {
-			log.Fatalf("%s: could not open snapshot %s", flag.CommandLine.Name(), res[0])
-		}
-		fmt.Printf("Uuid: %s\n", snap.Uuid)
-		fmt.Printf("CreationTime: %s\n", snap.CreationTime)
-		fmt.Printf("Version: %s\n", snap.Version)
-		fmt.Printf("Hostname: %s\n", snap.Hostname)
-		fmt.Printf("Username: %s\n", snap.Username)
-		fmt.Printf("Directories: %d\n", len(snap.Directories))
-		fmt.Printf("Files: %d\n", len(snap.Files))
-		fmt.Printf("NonRegular: %d\n", len(snap.NonRegular))
-		fmt.Printf("Sums: %d\n", len(snap.Pathnames))
-		fmt.Printf("Objects: %d\n", len(snap.Objects))
-		fmt.Printf("Chunks: %d\n", len(snap.Chunks))
-		fmt.Printf("Size: %s (%d bytes)\n", humanize.Bytes(snap.Size), snap.Size)
+	for _, snapshot := range snapshots {
+		fmt.Printf("Uuid: %s\n", snapshot.Uuid)
+		fmt.Printf("CreationTime: %s\n", snapshot.CreationTime)
+		fmt.Printf("Version: %s\n", snapshot.Version)
+		fmt.Printf("Hostname: %s\n", snapshot.Hostname)
+		fmt.Printf("Username: %s\n", snapshot.Username)
+		fmt.Printf("Directories: %d\n", len(snapshot.Directories))
+		fmt.Printf("Files: %d\n", len(snapshot.Files))
+		fmt.Printf("NonRegular: %d\n", len(snapshot.NonRegular))
+		fmt.Printf("Sums: %d\n", len(snapshot.Pathnames))
+		fmt.Printf("Objects: %d\n", len(snapshot.Objects))
+		fmt.Printf("Chunks: %d\n", len(snapshot.Chunks))
+		fmt.Printf("Size: %s (%d bytes)\n", humanize.Bytes(snapshot.Size), snapshot.Size)
 	}
 
 	return 0

1ae5586ffa9dae2b456c9b323c96dac7fbd05cd9
Gilles CHEHADE <gilles@poolp.org>
2021-11-29 04:11:22 +0100

    swap restic chunker with mine

--- cmd/plakar/plakar.go
+++ cmd/plakar/plakar.go
@@ -7,8 +7,11 @@ import (
 	"os"
 	"os/user"
 	"runtime"
+	"runtime/pprof"
 	"strings"
 	"time"
+
+	_ "net/http/pprof"
 
 	"github.com/poolpOrg/plakar/cache"
 	"github.com/poolpOrg/plakar/encryption"
@@ -83,7 +86,21 @@ func main() {
 	flag.BoolVar(&enableTracing, "trace", false, "enable tracing")
 	flag.BoolVar(&enableProfiling, "profile", false, "enable profiling")
 	flag.IntVar(&cpuCount, "cpu", cpuDefault, "limit the number of usable cores")
+	var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+	var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
 	flag.Parse()
+
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		defer f.Close() // error handling omitted for example
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
 
 	ctx.CommandLine = strings.Join(os.Args, " ")
 
@@ -237,5 +254,18 @@ func main() {
 	ctx.store.Close()
 
 	loggerWait()
+
+	if *memprofile != "" {
+		f, err := os.Create(*memprofile)
+		if err != nil {
+			log.Fatal("could not create memory profile: ", err)
+		}
+		defer f.Close() // error handling omitted for example
+		runtime.GC()    // get up-to-date statistics
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			log.Fatal("could not write memory profile: ", err)
+		}
+	}
+
 	os.Exit(exitCode)
 }
--- go.mod
+++ go.mod
@@ -11,7 +11,7 @@ require (
 	github.com/iafan/cwalk v0.0.0-20210125030640-586a8832a711
 	github.com/mattn/go-sqlite3 v1.14.9
 	github.com/pmezard/go-difflib v1.0.0
-	github.com/restic/chunker v0.4.0
+	github.com/poolpOrg/go-fastcdc v0.0.0-20211129023646-ac59994d3f1a
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
 	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
 	golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1

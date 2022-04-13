b889237c698ccb202fb316ddfddd7cb02a7a8f94
Gilles Chehade <gilles@poolp.org>
2022-04-13 17:17:30 +0200

    remove unused file, merge secret's handling in symmetric.go

--- encryption/symmetric.go
+++ encryption/symmetric.go
@@ -17,10 +17,39 @@
 package encryption
 
 import (
+	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+
+	"golang.org/x/crypto/pbkdf2"
 )
+
+func BuildSecretFromPassphrase(passphrase []byte) string {
+	salt := make([]byte, 16)
+	rand.Read(salt)
+	dk := pbkdf2.Key(passphrase, salt, 4096, 32, sha256.New)
+	secret := sha256.Sum256(dk)
+	return base64.StdEncoding.EncodeToString(append(salt, secret[:]...))
+}
+
+func DeriveSecret(passphrase []byte, secret string) ([]byte, error) {
+	decoded_secret, err := base64.StdEncoding.DecodeString(secret)
+	if err != nil {
+		return nil, err
+	}
+
+	salt, sum := decoded_secret[0:16], decoded_secret[16:]
+	dk := pbkdf2.Key(passphrase, salt, 4096, 32, sha256.New)
+	dksum := sha256.Sum256(dk)
+	if !bytes.Equal(dksum[:], sum) {
+		return nil, fmt.Errorf("passphrase does not match")
+	}
+	return dk, nil
+}
 
 func Encrypt(key []byte, buf []byte) ([]byte, error) {
 	subkey := make([]byte, 32)

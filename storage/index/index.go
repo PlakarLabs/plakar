c44bdf919a027a6460e02a7c6a97e7cef9e18f1c
Gilles Chehade <gilles@poolp.org>
2023-10-20 01:21:31 +0200

    prepare for packfile indexes loading

--- storage/index/index.go
+++ storage/index/index.go
@@ -17,6 +17,7 @@
 package index
 
 import (
+	"sync"
 	"time"
 
 	"github.com/PlakarLabs/plakar/logger"
@@ -25,10 +26,24 @@ import (
 )
 
 type Index struct {
+	muChecksums      sync.Mutex
+	checksumID       uint32
+	Checksums        map[[32]byte]uint32
+	checksumsInverse map[uint32][32]byte
+
+	muChunks sync.Mutex
+	Chunks   map[uint32]uint32
+
+	muObjects sync.Mutex
+	Objects   map[uint32]uint32
 }
 
 func NewIndex() *Index {
-	return &Index{}
+	return &Index{
+		Checksums: make(map[[32]byte]uint32),
+		Chunks:    make(map[uint32]uint32),
+		Objects:   make(map[uint32]uint32),
+	}
 }
 
 func NewFromBytes(serialized []byte) (*Index, error) {
@@ -42,6 +57,12 @@ func NewFromBytes(serialized []byte) (*Index, error) {
 	if err := msgpack.Unmarshal(serialized, &index); err != nil {
 		return nil, err
 	}
+
+	index.checksumsInverse = make(map[uint32][32]byte)
+	for checksum, checksumID := range index.Checksums {
+		index.checksumsInverse[checksumID] = checksum
+	}
+
 	return &index, nil
 }
 
@@ -57,4 +78,109 @@ func (index *Index) Serialize() ([]byte, error) {
 		return nil, err
 	}
 	return serialized, nil
+}
+
+func (index *Index) addChecksum(checksum [32]byte) uint32 {
+	index.muChecksums.Lock()
+	defer index.muChecksums.Unlock()
+
+	if checksumID, exists := index.Checksums[checksum]; !exists {
+		index.Checksums[checksum] = index.checksumID
+		index.checksumsInverse[index.checksumID] = checksum
+		checksumID = index.checksumID
+		index.checksumID++
+		return checksumID
+	} else {
+		return checksumID
+	}
+}
+
+func (index *Index) LookupChecksum(checksumID uint32) [32]byte {
+	index.muChecksums.Lock()
+	defer index.muChecksums.Unlock()
+
+	checksum, exists := index.checksumsInverse[checksumID]
+	if !exists {
+		panic("checksum not found")
+	}
+	return checksum
+}
+
+func (index *Index) Merge(deltaIndex *Index) {
+	index.muChecksums.Lock()
+	defer index.muChecksums.Unlock()
+
+	for deltaChecksum := range deltaIndex.Checksums {
+		if _, exists := index.Checksums[deltaChecksum]; !exists {
+			index.addChecksum(deltaChecksum)
+		}
+	}
+
+	for deltaChunkChecksumID, deltaPackfileChecksumID := range deltaIndex.Chunks {
+		index.SetPackfileForChunk(deltaIndex.LookupChecksum(deltaChunkChecksumID),
+			deltaIndex.LookupChecksum(deltaPackfileChecksumID))
+	}
+
+	for deltaObjectChecksumID, deltaPackfileChecksumID := range deltaIndex.Objects {
+		index.SetPackfileForObject(deltaIndex.LookupChecksum(deltaObjectChecksumID),
+			deltaIndex.LookupChecksum(deltaPackfileChecksumID))
+	}
+}
+
+func (index *Index) SetPackfileForChunk(packfileChecksum [32]byte, chunkChecksum [32]byte) {
+	index.muChunks.Lock()
+	defer index.muChunks.Unlock()
+
+	chunkID := index.addChecksum(chunkChecksum)
+	if _, exists := index.Chunks[chunkID]; !exists {
+		packfileID := index.addChecksum(packfileChecksum)
+		index.Chunks[chunkID] = packfileID
+	}
+}
+
+func (index *Index) GetPackfileForChunk(chunkChecksum [32]byte) ([32]byte, bool) {
+	index.muChunks.Lock()
+	defer index.muChunks.Unlock()
+
+	chunkID := index.addChecksum(chunkChecksum)
+	if packfileID, exists := index.Chunks[chunkID]; !exists {
+		return [32]byte{}, false
+	} else {
+		index.muChecksums.Lock()
+		packfileChecksum, exists := index.checksumsInverse[packfileID]
+		index.muChecksums.Unlock()
+		if !exists {
+			panic("packfile checksum not found")
+		}
+		return packfileChecksum, true
+	}
+}
+
+func (index *Index) SetPackfileForObject(packfileChecksum [32]byte, objectChecksum [32]byte) {
+	index.muObjects.Lock()
+	defer index.muObjects.Unlock()
+
+	objectID := index.addChecksum(objectChecksum)
+	if _, exists := index.Objects[objectID]; !exists {
+		packfileID := index.addChecksum(packfileChecksum)
+		index.Objects[objectID] = packfileID
+	}
+}
+
+func (index *Index) GetPackfileForObject(objectChecksum [32]byte) ([32]byte, bool) {
+	index.muObjects.Lock()
+	defer index.muObjects.Unlock()
+
+	objectID := index.addChecksum(objectChecksum)
+	if packfileID, exists := index.Objects[objectID]; !exists {
+		return [32]byte{}, false
+	} else {
+		index.muChecksums.Lock()
+		packfileChecksum, exists := index.checksumsInverse[packfileID]
+		index.muChecksums.Unlock()
+		if !exists {
+			panic("packfile checksum not found")
+		}
+		return packfileChecksum, true
+	}
 }

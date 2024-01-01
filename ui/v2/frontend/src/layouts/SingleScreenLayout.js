26a7d24cb7c19cc394852477773be9212a035975
Gilles Chehade <gilles@poolp.org>
2024-01-01 23:39:45 +0100

    do not use the ConfigShield, it is not useful for my need and is bugging me

--- ui/v2/frontend/src/layouts/SingleScreenLayout.js
+++ ui/v2/frontend/src/layouts/SingleScreenLayout.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import DefaultLayout from "./DefaultLayout";
 import {Stack} from "@mui/material";
-import ConfigShield from "../components/ConfigShield";
 
 function SingleScreenLayout({children, conf}) {
     return (
-        <ConfigShield>
-            <DefaultLayout conf={conf}>
-                <Stack sx={{p: 2}}>
-                    <Stack sx={{backgroundColor: 'white', p: 2, borderRadius: 2}}>
-                        {children}
-                    </Stack>
+        <DefaultLayout conf={conf}>
+            <Stack sx={{p: 2}}>
+                <Stack sx={{backgroundColor: 'white', p: 2, borderRadius: 2}}>
+                    {children}
                 </Stack>
-            </DefaultLayout>
-        </ConfigShield>
+            </Stack>
+        </DefaultLayout>
     )
 };
 
--- ui/v2/frontend/src/layouts/TwoColumnLayout.js
+++ ui/v2/frontend/src/layouts/TwoColumnLayout.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import DefaultLayout from "./DefaultLayout";
 import {Stack} from "@mui/material";
-import ConfigShield from "../components/ConfigShield";
+//import ConfigShield from "../components/ConfigShield";
 
 function TwoColumnLayout({leftComponent, rightComponent, conf}) {
     return (
-        <ConfigShield>
-            <DefaultLayout conf={conf}>
-                <Stack sx={{p: 2, height: '100%'}} direction={'row'}>
-                    <Stack sx={{mr: 1, backgroundColor: 'white', p: 2, borderRadius: 2, width: '70%'}}>
-                        {leftComponent}
-                    </Stack>
-                    <Stack sx={{ml: 1, backgroundColor: 'white', p: 2, borderRadius: 2, width: '30%'}}>
-                        {rightComponent}
-                    </Stack>
+        <DefaultLayout conf={conf}>
+            <Stack sx={{p: 2, height: '100%'}} direction={'row'}>
+                <Stack sx={{mr: 1, backgroundColor: 'white', p: 2, borderRadius: 2, width: '70%'}}>
+                    {leftComponent}
                 </Stack>
-            </DefaultLayout>
-        </ConfigShield>
+                <Stack sx={{ml: 1, backgroundColor: 'white', p: 2, borderRadius: 2, width: '30%'}}>
+                    {rightComponent}
+                </Stack>
+            </Stack>
+        </DefaultLayout>
     )
 }
 

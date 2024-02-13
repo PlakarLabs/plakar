f4335d6441bd0fe59e5ad724536a3563feeade36
Gilles Chehade <gilles@poolp.org>
2024-02-13 16:36:19 +0100

    commit build

---
+++ ui/v2/frontend/build/precache-manifest.e9856eb5ba291b1dd6f4f8eae36a7bc9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfa187ca8cb4ddcda25e6d29f418f10a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78f247ae8691aac6ce9f",
+    "url": "/static/css/main.4703b18a.chunk.css"
+  },
+  {
+    "revision": "46f3d137d81037917c7c",
+    "url": "/static/js/2.60ed0777.chunk.js"
+  },
+  {
+    "revision": "772c66f877889a93718e1a2497776fd3",
+    "url": "/static/js/2.60ed0777.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d8c7df7bdb8cb81e7c0",
+    "url": "/static/js/3.bb64005f.chunk.js"
+  },
+  {
+    "revision": "78f247ae8691aac6ce9f",
+    "url": "/static/js/main.40edc651.chunk.js"
+  },
+  {
+    "revision": "555a070fd93afb12690b",
+    "url": "/static/js/runtime-main.2c19cdb2.js"
+  },
+  {
+    "revision": "c31089ef3d7a4c3ecc13486a60ea89c1",
+    "url": "/static/media/Full.c31089ef.svg"
+  },
+  {
+    "revision": "90f7c44098c0b33782516a42179cef22",
+    "url": "/static/media/file.90f7c440.svg"
+  },
+  {
+    "revision": "97824aa49657cefa4abc5b8db4e07bb9",
+    "url": "/static/media/folder.97824aa4.svg"
+  }
+]);

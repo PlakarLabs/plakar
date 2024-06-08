2c453e20e57c0fdf0448e80b1dc3fdbf13752920
Gilles Chehade <gilles@poolp.org>
2024-06-08 14:14:52 +0200

    improve agent

--- agent/client.go
+++ agent/client.go
@@ -4,8 +4,11 @@ import (
 	"crypto/ed25519"
 	"encoding/base64"
 	"fmt"
+	"os"
+	"os/exec"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/poolpOrg/go-agentbuilder/client"
 	"github.com/poolpOrg/go-agentbuilder/protocol"
 )
@@ -14,6 +17,8 @@ type Client struct {
 	client     *client.Client
 	publicKey  ed25519.PublicKey
 	privateKey ed25519.PrivateKey
+
+	scheduler map[uuid.UUID]chan bool
 }
 
 func NewClient(address string, publicKey ed25519.PublicKey, privateKey ed25519.PrivateKey) *Client {
@@ -21,6 +26,8 @@ func NewClient(address string, publicKey ed25519.PublicKey, privateKey ed25519.P
 		client:     client.NewClient(address),
 		publicKey:  publicKey,
 		privateKey: privateKey,
+
+		scheduler: make(map[uuid.UUID]chan bool),
 	}
 }
 
@@ -33,6 +40,41 @@ func (c *Client) Run() {
 	}
 }
 
+func (c *Client) ConfigureTasks(tasks []Task) error {
+	// replace all existing tasks
+	tasksMap := make(map[uuid.UUID]struct{})
+
+	for _, task := range tasks {
+		tasksMap[task.ID] = struct{}{}
+		if _, ok := c.scheduler[task.ID]; ok {
+			close(c.scheduler[task.ID])
+		}
+		c.scheduler[task.ID] = make(chan bool)
+		go func(_task Task) {
+			fmt.Println("scheduling task", _task.Name, "every", _task.Interval.String(), "keep for", _task.Keep.String(), "=>", _task.Origin)
+			<-time.After(time.Until(_task.StartAT))
+			for {
+				select {
+				case <-c.scheduler[_task.ID]:
+					return
+				case <-time.After(_task.Interval):
+					fmt.Printf("[%s] %s: %s\n", time.Now().UTC(), _task.Name, _task.Origin)
+					exec.Command(os.Args[0], "push", "-tag", _task.Name, _task.Origin).Run()
+				}
+			}
+		}(task)
+	}
+
+	for taskID := range c.scheduler {
+		if _, ok := tasksMap[taskID]; !ok {
+			close(c.scheduler[taskID])
+			delete(c.scheduler, taskID)
+		}
+	}
+
+	return nil
+}
+
 func (c *Client) clientHandler(session *client.Session, incoming <-chan protocol.Packet) error {
 	for packet := range incoming {
 		switch req := packet.Payload.(type) {
@@ -43,9 +85,11 @@ func (c *Client) clientHandler(session *client.Session, incoming <-chan protocol
 			fmt.Println("server sent ping request")
 			packet.Response(NewResPing(req))
 		case ReqPushConfiguration:
-			fmt.Println("server sent agent configuration")
-			packet.Response(NewResOK())
-			//packet.Response(NewResKO(fmt.Errorf("not implemented")))
+			if err := c.ConfigureTasks(req.Tasks); err != nil {
+				packet.Response(NewResKO(err))
+			} else {
+				packet.Response(NewResOK())
+			}
 		default:
 			return fmt.Errorf("unexpected request type: %T", req)
 		}

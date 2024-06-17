4756acf122845d40f722b9d1341561cf3af1fd0d
Gilles Chehade <gilles@poolp.org>
2024-06-18 01:22:17 +0200

    remove UI v2 from main, move it back to a dedicated branch

--- ui/v2/frontend/src/components/Tag.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {Chip} from "@mui/material";
-
-const Tag = ({text}) => {
-    return (<Chip variant={'tag'} size={'small'} label={`# ${text}`}/>
-    );
-};
-
-export default Tag;

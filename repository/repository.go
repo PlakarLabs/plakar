a2b73106e1f882fb34c4a7de87547dfbf23d0574
Gilles Chehade <gilles@poolp.org>
2024-10-16 09:50:42 +0200

    encode and decode data in the repository layer

--- repository/repository.go
+++ repository/repository.go
@@ -1,15 +1,24 @@
 package repository
 
 import (
+	"time"
+
+	"github.com/PlakarLabs/plakar/compression"
+	"github.com/PlakarLabs/plakar/encryption"
+	"github.com/PlakarLabs/plakar/profiler"
 	"github.com/PlakarLabs/plakar/storage"
 )
 
 type Repository struct {
-	store *storage.Store
+	store         *storage.Store
+	configuration storage.Configuration
 }
 
 func New(store *storage.Store) *Repository {
-	return &Repository{store: store}
+	return &Repository{
+		store:         store,
+		configuration: store.Configuration(),
+	}
 }
 
 func (r *Repository) Store() *storage.Store {
@@ -17,5 +26,54 @@ func (r *Repository) Store() *storage.Store {
 }
 
 func (r *Repository) Configuration() storage.Configuration {
-	return r.store.Configuration()
+	return r.configuration
+}
+
+func (r *Repository) dataDecode(buffer []byte) ([]byte, error) {
+	t0 := time.Now()
+	defer func() {
+		profiler.RecordEvent("repository.dataDecode", time.Since(t0))
+	}()
+
+	if r.store.GetSecret() != nil {
+		tmp, err := encryption.Decrypt(r.store.GetSecret(), buffer)
+		if err != nil {
+			return nil, err
+		}
+		buffer = tmp
+	}
+
+	if r.configuration.Compression != "" {
+		tmp, err := compression.Inflate(r.configuration.Compression, buffer)
+		if err != nil {
+			return nil, err
+		}
+		buffer = tmp
+	}
+	return buffer, nil
+}
+
+func (r *Repository) dataEncode(buffer []byte) ([]byte, error) {
+	t0 := time.Now()
+	defer func() {
+		profiler.RecordEvent("repository.dataEncode", time.Since(t0))
+	}()
+
+	if r.configuration.Compression != "" {
+		tmp, err := compression.Deflate(r.configuration.Compression, buffer)
+		if err != nil {
+			return nil, err
+		}
+		buffer = tmp
+	}
+
+	if r.store.GetSecret() != nil {
+		tmp, err := encryption.Encrypt(r.store.GetSecret(), buffer)
+		if err != nil {
+			return nil, err
+		}
+		buffer = tmp
+	}
+
+	return buffer, nil
 }

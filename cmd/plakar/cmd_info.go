88614cee5c3d639560dc9c07782c0ed391b9f5a3
Gilles Chehade <gilles@poolp.org>
2024-10-25 17:20:47 +0200

    merge all inspection commands into info

--- cmd/plakar/cmd_info.go
+++ cmd/plakar/cmd_info.go
@@ -17,15 +17,25 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
 	"encoding/hex"
 	"flag"
 	"fmt"
+	"io"
 	"log"
+	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/PlakarLabs/plakar/context"
 	"github.com/PlakarLabs/plakar/logger"
+	"github.com/PlakarLabs/plakar/objects"
+	"github.com/PlakarLabs/plakar/packfile"
 	"github.com/PlakarLabs/plakar/repository"
+	"github.com/PlakarLabs/plakar/repository/state"
+	"github.com/PlakarLabs/plakar/snapshot/vfs"
 	"github.com/dustin/go-humanize"
 )
 
@@ -35,55 +45,72 @@ func init() {
 
 func cmd_info(ctx *context.Context, repo *repository.Repository, args []string) int {
 	if len(args) == 0 {
-		return info_plakar(repo)
+		return info_repository(repo)
 	}
 
 	flags := flag.NewFlagSet("info", flag.ExitOnError)
 	flags.Parse(args)
 
-	headers, err := getHeaders(repo, flags.Args())
-	if err != nil {
-		log.Fatal(err)
-	}
+	// Determine which concept to show information for based on flags.Args()[0]
+	switch flags.Arg(0) {
+	case "snapshot":
+		if len(flags.Args()) < 2 {
+			logger.Error("usage: %s snapshot snapshotID", flags.Name())
+			return 1
+		}
+		if err := info_snapshot(repo, flags.Args()[1]); err != nil {
+			logger.Error("error: %s", err)
+			return 1
+		}
 
-	for _, header := range headers {
-		indexID := header.GetIndexID()
-		fmt.Printf("IndexID: %s\n", hex.EncodeToString(indexID[:]))
-		fmt.Printf("CreationTime: %s\n", header.CreationTime)
-		fmt.Printf("CreationDuration: %s\n", header.CreationDuration)
+	case "state":
+		if err := info_state(repo, flags.Args()[1:]); err != nil {
+			logger.Error("error: %s", err)
+			return 1
+		}
 
-		fmt.Printf("Root: %x\n", header.Root)
-		fmt.Printf("Metadata: %x\n", header.Metadata)
-		fmt.Printf("Statistics: %x\n", header.Statistics)
+	case "packfile":
+		if err := info_packfile(repo, flags.Args()[1:]); err != nil {
+			logger.Error("error: %s", err)
+			return 1
+		}
 
-		fmt.Printf("Version: %s\n", repo.Configuration().Version)
-		fmt.Printf("Hostname: %s\n", header.Hostname)
-		fmt.Printf("Username: %s\n", header.Username)
-		fmt.Printf("CommandLine: %s\n", header.CommandLine)
-		fmt.Printf("OperatingSystem: %s\n", header.OperatingSystem)
-		fmt.Printf("Architecture: %s\n", header.Architecture)
-		fmt.Printf("NumCPU: %d\n", header.NumCPU)
+	case "object":
+		if len(flags.Args()) < 2 {
+			logger.Error("usage: %s object objectID", flags.Name())
+			return 1
+		}
+		if err := info_object(repo, flags.Args()[1]); err != nil {
+			logger.Error("error: %s", err)
+			return 1
+		}
 
-		fmt.Printf("MachineID: %s\n", header.MachineID)
-		fmt.Printf("PublicKey: %s\n", header.PublicKey)
-		fmt.Printf("Tags: %s\n", strings.Join(header.Tags, ", "))
-		fmt.Printf("Directories: %d\n", header.DirectoriesCount)
-		fmt.Printf("Files: %d\n", header.FilesCount)
+	case "vfs":
+		if len(flags.Args()) < 2 {
+			logger.Error("usage: %s vfs snapshotPathname", flags.Name())
+			return 1
+		}
+		if err := info_vfs(repo, flags.Args()[1]); err != nil {
+			logger.Error("error: %s", err)
+			return 1
+		}
 
-		fmt.Printf("Snapshot.Size: %s (%d bytes)\n", humanize.Bytes(header.ScanProcessedSize), header.ScanProcessedSize)
+	default:
+		fmt.Println("Invalid parameter. usage: info [repository|snapshot|object|chunk|state|packfile|vfs]")
+		return 1
 	}
 
 	return 0
 }
 
-func info_plakar(repo *repository.Repository) int {
+func info_repository(repo *repository.Repository) int {
 	metadatas, err := getHeaders(repo, nil)
 	if err != nil {
 		logger.Warn("%s", err)
 		return 1
 	}
 
-	fmt.Println("StoreID:", repo.Configuration().StoreID)
+	fmt.Println("RepositoryID:", repo.Configuration().StoreID)
 	fmt.Printf("CreationTime: %s\n", repo.Configuration().CreationTime)
 	fmt.Println("Version:", repo.Configuration().Version)
 
@@ -122,4 +149,353 @@ func info_plakar(repo *repository.Repository) int {
 	fmt.Printf("Metadata Size: %s (%d bytes)\n", humanize.Bytes(totalMetadataSize), totalMetadataSize)
 
 	return 0
+}
+
+func info_snapshot(repo *repository.Repository, snapshotID string) error {
+
+	snap, err := openSnapshotByPrefix(repo, snapshotID)
+	if err != nil {
+		return err
+	}
+
+	header := snap.Header
+
+	indexID := header.GetIndexID()
+	fmt.Printf("IndexID: %s\n", hex.EncodeToString(indexID[:]))
+	fmt.Printf("CreationTime: %s\n", header.CreationTime)
+	fmt.Printf("CreationDuration: %s\n", header.CreationDuration)
+
+	fmt.Printf("Root: %x\n", header.Root)
+	fmt.Printf("Metadata: %x\n", header.Metadata)
+	fmt.Printf("Statistics: %x\n", header.Statistics)
+
+	fmt.Printf("Version: %s\n", repo.Configuration().Version)
+	fmt.Printf("Hostname: %s\n", header.Hostname)
+	fmt.Printf("Username: %s\n", header.Username)
+	fmt.Printf("CommandLine: %s\n", header.CommandLine)
+	fmt.Printf("OperatingSystem: %s\n", header.OperatingSystem)
+	fmt.Printf("Architecture: %s\n", header.Architecture)
+	fmt.Printf("NumCPU: %d\n", header.NumCPU)
+
+	fmt.Printf("MachineID: %s\n", header.MachineID)
+	fmt.Printf("PublicKey: %s\n", header.PublicKey)
+	fmt.Printf("Tags: %s\n", strings.Join(header.Tags, ", "))
+	fmt.Printf("Directories: %d\n", header.DirectoriesCount)
+	fmt.Printf("Files: %d\n", header.FilesCount)
+
+	fmt.Printf("Snapshot.Size: %s (%d bytes)\n", humanize.Bytes(header.ScanProcessedSize), header.ScanProcessedSize)
+	return nil
+}
+
+func info_state(repo *repository.Repository, args []string) error {
+	if len(args) == 0 {
+		states, err := repo.GetStates()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, state := range states {
+			fmt.Printf("%x\n", state)
+		}
+	} else {
+		for _, arg := range args {
+			// convert arg to [32]byte
+			if len(arg) != 64 {
+				log.Fatalf("invalid packfile hash: %s", arg)
+			}
+
+			b, err := hex.DecodeString(arg)
+			if err != nil {
+				log.Fatalf("invalid packfile hash: %s", arg)
+			}
+
+			// Convert the byte slice to a [32]byte
+			var byteArray [32]byte
+			copy(byteArray[:], b)
+
+			rawState, _, err := repo.GetState(byteArray)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			st, err := state.NewFromBytes(rawState)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Printf("Version: %d.%d.%d\n", st.Metadata.Version/100, (st.Metadata.Version/10)%10, st.Metadata.Version%10)
+			fmt.Printf("Creation: %s\n", st.Metadata.CreationTime)
+			if len(st.Metadata.Extends) > 0 {
+				fmt.Printf("Extends:\n")
+				for _, stateID := range st.Metadata.Extends {
+					fmt.Printf("  %x\n", stateID)
+				}
+			}
+
+			for snapshotID, subpart := range st.Snapshots {
+				fmt.Printf("snapshot %x : packfile %x, offset %d, length %d\n",
+					st.IdToChecksum[snapshotID],
+					st.IdToChecksum[subpart.Packfile],
+					subpart.Offset,
+					subpart.Length)
+			}
+
+			for chunk, subpart := range st.Chunks {
+				fmt.Printf("chunk %x : packfile %x, offset %d, length %d\n",
+					st.IdToChecksum[chunk],
+					st.IdToChecksum[subpart.Packfile],
+					subpart.Offset,
+					subpart.Length)
+			}
+
+			for object, subpart := range st.Objects {
+				fmt.Printf("object %x : packfile %x, offset %d, length %d\n",
+					st.IdToChecksum[object],
+					st.IdToChecksum[subpart.Packfile],
+					subpart.Offset,
+					subpart.Length)
+			}
+
+			for file, subpart := range st.Files {
+				fmt.Printf("file %x : packfile %x, offset %d, length %d\n",
+					st.IdToChecksum[file],
+					st.IdToChecksum[subpart.Packfile],
+					subpart.Offset,
+					subpart.Length)
+			}
+
+			for directory, subpart := range st.Directories {
+				fmt.Printf("directory %x : packfile %x, offset %d, length %d\n",
+					st.IdToChecksum[directory],
+					st.IdToChecksum[subpart.Packfile],
+					subpart.Offset,
+					subpart.Length)
+			}
+
+			for data, subpart := range st.Datas {
+				fmt.Printf("data %x : packfile %x, offset %d, length %d\n",
+					st.IdToChecksum[data],
+					st.IdToChecksum[subpart.Packfile],
+					subpart.Offset,
+					subpart.Length)
+			}
+		}
+	}
+	return nil
+}
+
+func info_packfile(repo *repository.Repository, args []string) error {
+	if len(args) == 0 {
+		packfiles, err := repo.GetPackfiles()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, packfile := range packfiles {
+			fmt.Printf("%x\n", packfile)
+		}
+	} else {
+		for _, arg := range args {
+			// convert arg to [32]byte
+			if len(arg) != 64 {
+				log.Fatalf("invalid packfile hash: %s", arg)
+			}
+
+			b, err := hex.DecodeString(arg)
+			if err != nil {
+				log.Fatalf("invalid packfile hash: %s", arg)
+			}
+
+			// Convert the byte slice to a [32]byte
+			var byteArray [32]byte
+			copy(byteArray[:], b)
+
+			rd, _, err := repo.GetPackfile(byteArray)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			rawPackfile, err := io.ReadAll(rd)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			versionBytes := rawPackfile[len(rawPackfile)-5 : len(rawPackfile)-5+4]
+			version := binary.LittleEndian.Uint32(versionBytes)
+
+			//			version := rawPackfile[len(rawPackfile)-2]
+			footerOffset := rawPackfile[len(rawPackfile)-1]
+			rawPackfile = rawPackfile[:len(rawPackfile)-5]
+
+			_ = version
+
+			footerbuf := rawPackfile[len(rawPackfile)-int(footerOffset):]
+			rawPackfile = rawPackfile[:len(rawPackfile)-int(footerOffset)]
+
+			footerbuf, err = repo.Decode(footerbuf)
+			if err != nil {
+				log.Fatal(err)
+			}
+			footer, err := packfile.NewFooterFromBytes(footerbuf)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			indexbuf := rawPackfile[int(footer.IndexOffset):]
+			rawPackfile = rawPackfile[:int(footer.IndexOffset)]
+
+			indexbuf, err = repo.Decode(indexbuf)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			hasher := sha256.New()
+			hasher.Write(indexbuf)
+
+			if !bytes.Equal(hasher.Sum(nil), footer.IndexChecksum[:]) {
+				log.Fatal("index checksum mismatch")
+			}
+
+			rawPackfile = append(rawPackfile, indexbuf...)
+			rawPackfile = append(rawPackfile, footerbuf...)
+
+			p, err := packfile.NewFromBytes(rawPackfile)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Printf("Version: %d.%d.%d\n", p.Footer.Version/100, p.Footer.Version%100/10, p.Footer.Version%10)
+			fmt.Printf("Timestamp: %s\n", time.Unix(0, p.Footer.Timestamp))
+			fmt.Printf("Index checksum: %x\n", p.Footer.IndexChecksum)
+			fmt.Println()
+
+			for i, entry := range p.Index {
+				fmt.Printf("blob[%d]: %x %d %d %s\n", i, entry.Checksum, entry.Offset, entry.Length, entry.TypeName())
+			}
+		}
+	}
+	return nil
+}
+
+func info_object(repo *repository.Repository, objectID string) error {
+	if len(objectID) != 64 {
+		log.Fatalf("invalid object hash: %s", objectID)
+	}
+
+	b, err := hex.DecodeString(objectID)
+	if err != nil {
+		log.Fatalf("invalid object hash: %s", objectID)
+	}
+
+	// Convert the byte slice to a [32]byte
+	var byteArray [32]byte
+	copy(byteArray[:], b)
+
+	packfileID, offset, length, exists := repo.State().GetSubpartForObject(byteArray)
+	if !exists {
+		log.Fatal(err)
+	}
+
+	rd, _, err := repo.GetPackfileBlob(packfileID, offset, length)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	blob, err := io.ReadAll(rd)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	object, err := objects.NewObjectFromBytes(blob)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("object: %x\n", object.Checksum)
+	fmt.Println("  type:", object.ContentType)
+	if len(object.Tags) > 0 {
+		fmt.Println("  tags:", strings.Join(object.Tags, ","))
+	}
+
+	fmt.Println("  chunks:")
+	for _, chunk := range object.Chunks {
+		fmt.Printf("    checksum: %x\n", chunk.Checksum)
+	}
+	return nil
+}
+
+func info_vfs(repo *repository.Repository, snapshotPath string) error {
+	// TODO
+	snapshotPrefix, pathname := parseSnapshotID(snapshotPath)
+	snap1, err := openSnapshotByPrefix(repo, snapshotPrefix)
+	if err != nil {
+		return err
+	}
+	fs, err := snap1.Filesystem()
+	if err != nil {
+		return err
+	}
+
+	fsinfo, err := fs.Stat(filepath.Clean(pathname))
+	if err != nil {
+		return err
+	}
+
+	if dirEntry, isDir := fsinfo.(*vfs.DirEntry); isDir {
+		fmt.Printf("[DirEntry]\n")
+		fmt.Printf("Version: %d\n", dirEntry.Version)
+		fmt.Printf("ParentPath: %s\n", dirEntry.ParentPath)
+		fmt.Printf("Name: %s\n", dirEntry.FileInfo().Name())
+		fmt.Printf("Type: %d\n", dirEntry.Type)
+		fmt.Printf("Size: %d\n", dirEntry.FileInfo().Size())
+		fmt.Printf("Permissions: %s\n", dirEntry.FileInfo().Mode())
+		fmt.Printf("ModTime: %s\n", dirEntry.FileInfo().ModTime())
+		fmt.Printf("DeviceID: %d\n", dirEntry.DeviceID)
+		fmt.Printf("InodeID: %d\n", dirEntry.InodeID)
+		fmt.Printf("UserID: %d\n", dirEntry.UserID)
+		fmt.Printf("GroupID: %d\n", dirEntry.GroupID)
+		fmt.Printf("NumLinks: %d\n", dirEntry.NumLinks)
+		fmt.Printf("ExtendedAttributes: %s\n", dirEntry.ExtendedAttributes)
+		fmt.Printf("CustomMetadata: %s\n", dirEntry.CustomMetadata)
+		fmt.Printf("Tags: %s\n", dirEntry.Tags)
+		for offset, child := range dirEntry.Children {
+			fmt.Printf("Child[%d].Checksum: %x\n", offset, child.Checksum)
+			fmt.Printf("Child[%d].FileInfo.Name(): %s\n", offset, child.FileInfo.Name())
+			fmt.Printf("Child[%d].FileInfo.Size(): %d\n", offset, child.FileInfo.Size())
+			fmt.Printf("Child[%d].FileInfo.Mode(): %s\n", offset, child.FileInfo.Mode())
+			fmt.Printf("Child[%d].FileInfo.Dev(): %d\n", offset, child.FileInfo.Dev())
+			fmt.Printf("Child[%d].FileInfo.Ino(): %d\n", offset, child.FileInfo.Ino())
+			fmt.Printf("Child[%d].FileInfo.Uid(): %d\n", offset, child.FileInfo.Uid())
+			fmt.Printf("Child[%d].FileInfo.Gid(): %d\n", offset, child.FileInfo.Gid())
+			fmt.Printf("Child[%d].FileInfo.Nlink(): %d\n", offset, child.FileInfo.Nlink())
+		}
+
+	} else if fileEntry, isFile := fsinfo.(*vfs.FileEntry); isFile {
+		fmt.Printf("[FileEntry]\n")
+		fmt.Printf("Version: %d\n", fileEntry.Version)
+		fmt.Printf("ParentPath: %s\n", fileEntry.ParentPath)
+		fmt.Printf("Name: %s\n", fileEntry.FileInfo().Name())
+		fmt.Printf("Type: %d\n", fileEntry.Type)
+		fmt.Printf("Size: %d\n", fileEntry.FileInfo().Size())
+		fmt.Printf("Permissions: %s\n", fileEntry.FileInfo().Mode())
+		fmt.Printf("ModTime: %s\n", fileEntry.FileInfo().ModTime())
+		fmt.Printf("DeviceID: %d\n", fileEntry.DeviceID)
+		fmt.Printf("InodeID: %d\n", fileEntry.InodeID)
+		fmt.Printf("UserID: %d\n", fileEntry.UserID)
+		fmt.Printf("GroupID: %d\n", fileEntry.GroupID)
+		fmt.Printf("NumLinks: %d\n", fileEntry.NumLinks)
+		fmt.Printf("ExtendedAttributes: %s\n", fileEntry.ExtendedAttributes)
+		fmt.Printf("FileAttributes: %v\n", fileEntry.FileAttributes)
+		if fileEntry.SymlinkTarget != "" {
+			fmt.Printf("SymlinkTarget: %s\n", fileEntry.SymlinkTarget)
+		}
+		fmt.Printf("CustomMetadata: %s\n", fileEntry.CustomMetadata)
+		fmt.Printf("Tags: %s\n", fileEntry.Tags)
+		fmt.Printf("Checksum: %x\n", fileEntry.Checksum)
+		for offset, chunk := range fileEntry.Chunks {
+			fmt.Printf("Chunk[%d].Checksum: %x\n", offset, chunk.Checksum)
+			fmt.Printf("Chunk[%d].Length: %d\n", offset, chunk.Length)
+		}
+	}
+	return nil
 }
--- cmd/plakar/cmd_sync.go
+++ cmd/plakar/cmd_sync.go
@@ -18,17 +18,10 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"os"
-	"sync"
 
 	"github.com/PlakarLabs/plakar/context"
-	"github.com/PlakarLabs/plakar/encryption"
 	"github.com/PlakarLabs/plakar/logger"
 	"github.com/PlakarLabs/plakar/repository"
-	"github.com/PlakarLabs/plakar/snapshot"
-	"github.com/PlakarLabs/plakar/storage"
-	"github.com/vmihailenco/msgpack/v5"
 )
 
 func init() {
@@ -57,189 +50,196 @@ func cmd_sync(ctx *context.Context, repo *repository.Repository, args []string) 
 		return 1
 	}
 
-	var srcStorage *storage.Store
-	var dstStorage *storage.Store
-	var targetStorage *storage.Store
+	_ = snapshotID
+	_ = direction
+	_ = syncRepository
 
-	var srcRepository *repository.Repository
-	var dstRepository *repository.Repository
-	var targetRepository *repository.Repository
+	/*
 
-	var err error
-	if direction == "to" {
-		srcRepository = repo
-		dstStorage, err = storage.Open(ctx, syncRepository)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s: could not open repository: %s\n", syncRepository, err)
+		var srcStorage *storage.Store
+		var dstStorage *storage.Store
+		var targetStorage *storage.Store
+
+		var srcRepository *repository.Repository
+		var dstRepository *repository.Repository
+		var targetRepository *repository.Repository
+
+		var err error
+		if direction == "to" {
+			srcRepository = repo
+			dstStorage, err = storage.Open(ctx, syncRepository)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%s: could not open repository: %s\n", syncRepository, err)
+				return 1
+			}
+			targetStorage = dstStorage
+		} else if direction == "from" {
+			dstRepository = repo
+			srcStorage, err = storage.Open(ctx, syncRepository)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%s: could not open repository: %s\n", syncRepository, err)
+				return 1
+			}
+			targetStorage = srcStorage
+		} else {
+			logger.Error("usage: %s [snapshotID] to|from repository", flags.Name())
 			return 1
 		}
-		targetStorage = dstStorage
-	} else if direction == "from" {
-		dstRepository = repo
-		srcStorage, err = storage.Open(ctx, syncRepository)
+
+		var targetSecret []byte
+		if targetStorage.Configuration().Encryption != "" {
+			for {
+				passphrase, err := getPassphrase("destination repository")
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "%s\n", err)
+					continue
+				}
+
+				secret, err := encryption.DeriveSecret(passphrase, targetStorage.Configuration().EncryptionKey)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "%s\n", err)
+					continue
+				}
+				targetSecret = secret
+				break
+			}
+		}
+		targetRepository, err = repository.New(targetStorage, targetSecret)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s: could not open repository: %s\n", syncRepository, err)
+			fmt.Fprintf(os.Stderr, "%s: could not open repository: %s\n", targetStorage.Location(), err)
 			return 1
 		}
-		targetStorage = srcStorage
-	} else {
-		logger.Error("usage: %s [snapshotID] to|from repository", flags.Name())
-		return 1
-	}
 
-	var targetSecret []byte
-	if targetStorage.Configuration().Encryption != "" {
-		for {
-			passphrase, err := getPassphrase("destination repository")
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "%s\n", err)
-				continue
-			}
-
-			secret, err := encryption.DeriveSecret(passphrase, targetStorage.Configuration().EncryptionKey)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "%s\n", err)
-				continue
-			}
-			targetSecret = secret
-			break
+		if targetStorage == srcStorage {
+			srcRepository = targetRepository
+		} else {
+			dstRepository = targetRepository
 		}
-	}
-	targetRepository, err = repository.New(targetStorage, targetSecret)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s: could not open repository: %s\n", targetStorage.Location(), err)
-		return 1
-	}
 
-	if targetStorage == srcStorage {
-		srcRepository = targetRepository
-	} else {
-		dstRepository = targetRepository
-	}
+		var muChunkChecksum sync.Mutex
+		chunkChecksum := make(map[[32]byte]bool)
 
-	var muChunkChecksum sync.Mutex
-	chunkChecksum := make(map[[32]byte]bool)
+		var muObjectChecksum sync.Mutex
+		objectChecksum := make(map[[32]byte]bool)
 
-	var muObjectChecksum sync.Mutex
-	objectChecksum := make(map[[32]byte]bool)
-
-	sourceIndexes, err := srcRepository.GetSnapshots()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s: could not get indexes list from repository: %s\n", srcRepository.Location(), err)
-		return 1
-	}
-
-	destIndexes, err := dstRepository.GetSnapshots()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s: could not get indexes list from repository: %s\n", dstRepository.Location(), err)
-		return 1
-	}
-
-	syncIndexes := make([][32]byte, 0)
-
-	for _, index := range findSnapshotByPrefix(sourceIndexes, snapshotID) {
-		if !indexArrayContains(destIndexes, index) {
-			syncIndexes = append(syncIndexes, index)
+		sourceIndexes, err := srcRepository.GetSnapshots()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s: could not get indexes list from repository: %s\n", srcRepository.Location(), err)
+			return 1
 		}
-	}
 
-	wg := sync.WaitGroup{}
-	for _, _indexID := range syncIndexes {
-		wg.Add(1)
-		go func(indexID [32]byte) {
-			defer wg.Done()
-			sourceSnapshot, err := snapshot.Load(srcRepository, indexID)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "%s: could not load snapshot from repository: %s\n", srcRepository.Location(), err)
-				return
+		destIndexes, err := dstRepository.GetSnapshots()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s: could not get indexes list from repository: %s\n", dstRepository.Location(), err)
+			return 1
+		}
+
+		syncIndexes := make([][32]byte, 0)
+
+		for _, index := range findSnapshotByPrefix(sourceIndexes, snapshotID) {
+			if !indexArrayContains(destIndexes, index) {
+				syncIndexes = append(syncIndexes, index)
 			}
+		}
 
-			copySnapshot, err := snapshot.New(dstRepository, indexID)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "%s: could not create snapshot in repository: %s\n", syncRepository, err)
-				return
-			}
+		wg := sync.WaitGroup{}
+		for _, _indexID := range syncIndexes {
+			wg.Add(1)
+			go func(indexID [32]byte) {
+				defer wg.Done()
+				sourceSnapshot, err := snapshot.Load(srcRepository, indexID)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "%s: could not load snapshot from repository: %s\n", srcRepository.Location(), err)
+					return
+				}
 
-			// rebuild a new snapshot w/ identical fs, but destination specific index and rebuilt metadata
-			// should share same UUID but take into account configuration differnces
-			copySnapshot.Header = sourceSnapshot.Header
-			copySnapshot.Metadata = sourceSnapshot.Metadata
+				copySnapshot, err := snapshot.New(dstRepository, indexID)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "%s: could not create snapshot in repository: %s\n", syncRepository, err)
+					return
+				}
 
-			wg2 := sync.WaitGroup{}
-			for _chunkID := range sourceSnapshot.Repository().State().ListChunks() {
-				wg2.Add(1)
-				go func(chunkID [32]byte) {
-					defer wg2.Done()
-					muChunkChecksum.Lock()
-					_, exists := chunkChecksum[chunkID]
-					muChunkChecksum.Unlock()
-					if !exists {
-						exists := copySnapshot.CheckChunk(chunkID)
-						if !exists {
-							data, err := sourceSnapshot.GetChunk(chunkID)
-							if err != nil {
-								fmt.Fprintf(os.Stderr, "%s: could not get chunk from repository: %s\n", srcRepository.Location(), err)
-								return
-							}
-							err = copySnapshot.PutChunk(chunkID, data)
-							if err != nil {
-								fmt.Fprintf(os.Stderr, "%s: could not put chunk to repository: %s\n", syncRepository, err)
-								return
-							}
-						}
+				// rebuild a new snapshot w/ identical fs, but destination specific index and rebuilt metadata
+				// should share same UUID but take into account configuration differnces
+				copySnapshot.Header = sourceSnapshot.Header
+				copySnapshot.Metadata = sourceSnapshot.Metadata
+
+				wg2 := sync.WaitGroup{}
+				for _chunkID := range sourceSnapshot.Repository().State().ListChunks() {
+					wg2.Add(1)
+					go func(chunkID [32]byte) {
+						defer wg2.Done()
 						muChunkChecksum.Lock()
-						chunkChecksum[chunkID] = true
+						_, exists := chunkChecksum[chunkID]
 						muChunkChecksum.Unlock()
-					}
-				}(_chunkID)
-			}
-			wg2.Wait()
-
-			wg3 := sync.WaitGroup{}
-			for _objectID := range sourceSnapshot.Repository().State().ListObjects() {
-				wg3.Add(1)
-				go func(objectID [32]byte) {
-					defer wg3.Done()
-					muObjectChecksum.Lock()
-					_, exists := objectChecksum[objectID]
-					muObjectChecksum.Unlock()
-
-					if !exists {
-						exists := copySnapshot.CheckObject(objectID)
 						if !exists {
-							object, err := sourceSnapshot.LookupObject(objectID)
-							if err != nil {
-								fmt.Fprintf(os.Stderr, "%s: could not get object from repository: %s\n", srcRepository.Location(), err)
-								return
+							exists := copySnapshot.CheckChunk(chunkID)
+							if !exists {
+								data, err := sourceSnapshot.GetChunk(chunkID)
+								if err != nil {
+									fmt.Fprintf(os.Stderr, "%s: could not get chunk from repository: %s\n", srcRepository.Location(), err)
+									return
+								}
+								err = copySnapshot.PutChunk(chunkID, data)
+								if err != nil {
+									fmt.Fprintf(os.Stderr, "%s: could not put chunk to repository: %s\n", syncRepository, err)
+									return
+								}
 							}
-
-							data, err := msgpack.Marshal(object)
-							if err != nil {
-								fmt.Fprintf(os.Stderr, "%s: could not marshal object: %s\n", syncRepository, err)
-								return
-							}
-
-							err = copySnapshot.PutObject(object.Checksum, data)
-							if err != nil {
-								fmt.Fprintf(os.Stderr, "%s: could not put object to repository: %s\n", syncRepository, err)
-								return
-							}
+							muChunkChecksum.Lock()
+							chunkChecksum[chunkID] = true
+							muChunkChecksum.Unlock()
 						}
-						muObjectChecksum.Lock()
-						objectChecksum[objectID] = true
-						muObjectChecksum.Unlock()
-					}
-				}(_objectID)
-			}
-			wg3.Wait()
+					}(_chunkID)
+				}
+				wg2.Wait()
 
-			err = copySnapshot.Commit()
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "%s: could not commit object to repository: %s\n", syncRepository, err)
-				return
-			}
-		}(_indexID)
-	}
-	wg.Wait()
+				wg3 := sync.WaitGroup{}
+				for _objectID := range sourceSnapshot.Repository().State().ListObjects() {
+					wg3.Add(1)
+					go func(objectID [32]byte) {
+						defer wg3.Done()
+						muObjectChecksum.Lock()
+						_, exists := objectChecksum[objectID]
+						muObjectChecksum.Unlock()
+
+						if !exists {
+							exists := copySnapshot.CheckObject(objectID)
+							if !exists {
+								object, err := sourceSnapshot.LookupObject(objectID)
+								if err != nil {
+									fmt.Fprintf(os.Stderr, "%s: could not get object from repository: %s\n", srcRepository.Location(), err)
+									return
+								}
+
+								data, err := msgpack.Marshal(object)
+								if err != nil {
+									fmt.Fprintf(os.Stderr, "%s: could not marshal object: %s\n", syncRepository, err)
+									return
+								}
+
+								err = copySnapshot.PutObject(object.Checksum, data)
+								if err != nil {
+									fmt.Fprintf(os.Stderr, "%s: could not put object to repository: %s\n", syncRepository, err)
+									return
+								}
+							}
+							muObjectChecksum.Lock()
+							objectChecksum[objectID] = true
+							muObjectChecksum.Unlock()
+						}
+					}(_objectID)
+				}
+				wg3.Wait()
+
+				err = copySnapshot.Commit()
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "%s: could not commit object to repository: %s\n", syncRepository, err)
+					return
+				}
+			}(_indexID)
+		}
+		wg.Wait()
+	*/
 	return 0
 }

91cebe0a58dc556a0f814db614f1192e59e7bc9c
Gilles Chehade <gilles@poolp.org>
2024-11-03 01:21:44 +0100

    only allow sha256, support for blake3 will require more work

--- hashing/hashing.go
+++ hashing/hashing.go
@@ -3,8 +3,6 @@ package hashing
 import (
 	"crypto/sha256"
 	"hash"
-
-	"github.com/zeebo/blake3"
 )
 
 func DefaultAlgorithm() string {
@@ -15,8 +13,6 @@ func GetHasher(name string) hash.Hash {
 	switch name {
 	case "sha256":
 		return sha256.New()
-	case "blake3":
-		return blake3.New()
 	default:
 		return nil
 	}
--- hashing/hashing_test.go
+++ hashing/hashing_test.go
@@ -2,8 +2,6 @@ package hashing
 
 import (
 	"testing"
-
-	"github.com/zeebo/blake3"
 )
 
 func TestDefaultAlgorithm(t *testing.T) {
@@ -19,15 +17,6 @@ func TestGetHasher(t *testing.T) {
 	hasher := GetHasher("sha256")
 	if hasher == nil {
 		t.Error("Expected sha256 hasher, but got nil")
-	}
-
-	// Test for blake3
-	hasher = GetHasher("blake3")
-	if hasher == nil {
-		t.Error("Expected blake3 hasher, but got nil")
-	}
-	if _, ok := hasher.(*blake3.Hasher); !ok {
-		t.Errorf("Expected blake3 hasher type, but got %T", hasher)
 	}
 
 	// Test for unknown algorithm

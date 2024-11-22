63cbf1d6b35bc1cae4e1460b7aecfb3096a2dfee
Julien Castets <castets.j@gmail.com>
2024-11-22 15:01:25 +0100

    Update proto

--- cmd/testclient/main.go
+++ cmd/testclient/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"io"
 
 	"github.com/PlakarKorp/go-plakar-sdk/pkg/importer"
 	"google.golang.org/grpc"
@@ -21,11 +23,36 @@ func main() {
 
 	client := importer.NewImporterClient(conn)
 
+	type_, err := client.Type(context.Background(), &importer.TypeRequest{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Importer type: %v\n", type_.Type)
+
 	origin, err := client.Origin(context.Background(), &importer.OriginRequest{})
 	if err != nil {
 		panic(err)
 	}
+	fmt.Printf("Importer origin: %v\n", origin.Origin)
 
-	fmt.Printf("%v\n", origin.Origin)
+	root, err := client.Root(context.Background(), &importer.RootRequest{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Importer root: %v\n", root.Root)
 
+	scanStream, err := client.Scan(context.Background(), &importer.ScanRequest{})
+	if err != nil {
+		panic(err)
+	}
+	for {
+		resp, err := scanStream.Recv()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			panic(err)
+		}
+		fmt.Printf("stream=%v\n", resp)
+	}
 }

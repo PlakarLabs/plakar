26fb920ed56348e9ff9e01218e1a8cfd5c850632
Omar Polo <op@omarpolo.com>
2024-12-06 10:04:41 +0000

    btree.Storer impl. for leveldb cache and snapshots

---
+++ caching/store.go
@@ -0,0 +1,42 @@
+package caching
+
+import (
+	"fmt"
+
+	"github.com/PlakarKorp/plakar/snapshot/btree"
+	"github.com/vmihailenco/msgpack/v5"
+)
+
+// DBStore implements btree.Storer
+type DBStore[K any, V any] struct {
+	Prefix string
+	idx    int
+	Cache  *ScanCache
+}
+
+func (ds *DBStore[K, V]) Get(idx int) (node btree.Node[K, int, V], err error) {
+	bytes, err := ds.Cache.get(ds.Prefix, fmt.Sprint(idx))
+	if err != nil {
+		return
+	}
+	err = msgpack.Unmarshal(bytes, &node)
+	return
+}
+
+func (ds *DBStore[K, V]) Update(idx int, node btree.Node[K, int, V]) error {
+	bytes, err := msgpack.Marshal(&node)
+	if err != nil {
+		return err
+	}
+	return ds.Cache.put(ds.Prefix, fmt.Sprint(idx), bytes)
+}
+
+func (ds *DBStore[K, V]) Put(node btree.Node[K, int, V]) (int, error) {
+	ds.idx++
+	idx := ds.idx
+	bytes, err := msgpack.Marshal(&node)
+	if err != nil {
+		return 0, err
+	}
+	return idx, ds.Cache.put(ds.Prefix, fmt.Sprint(idx), bytes)
+}
